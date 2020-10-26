aaa8462b8ef42b1aa2bd39ad23220238e866f827
root <root@f1b8df2b9f98>
2020-10-26 20:28:28 +0000

    Dragon fight!

--- src/_03_print_and_popups/_3_world_domination/WorldDomination.java
+++ src/_03_print_and_popups/_3_world_domination/WorldDomination.java
@@ -8,11 +8,16 @@ public class WorldDomination {
 	public static void main(String[] args) {
 		
 		// 1. Ask the user if they know how to write code.
-
+		String answer = JOptionPane.showInputDialog("Do you know how to write code?");
+		
 		// 2. If they say "yes", tell them they will rule the world.
-
+		if (answer.equalsIgnoreCase("Yes")) {
+		JOptionPane.showMessageDialog(null, "Then you will rule the world.");
+		}
 		// 3. Otherwise, tell them to sign up for classes at The League.
-
+		else {
+		JOptionPane.showMessageDialog(null, "Sign up for classes at The League.");
+			}
 	}
 }
 
